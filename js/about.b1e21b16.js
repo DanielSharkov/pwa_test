cd2c22c72e90cf5dc621f1677aa1617f03e762e7
Daniel Sharkov <scharktv@gmail.com>
2018-11-06 13:08:12 +0100

    Update look and functionality

---
+++ js/about.b1e21b16.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{"754b":function(e,t,n){"use strict";n.r(t);var a=function(){var e=this,t=e.$createElement;e._self._c;return e._m(0)},o=[function(){var e=this,t=e.$createElement,n=e._self._c||t;return n("div",{attrs:{id:"about-view"}},[n("h1",[e._v("This is an about page")]),n("span",[e._v("Here's some more info for a better preview of this awesome fancy site.")])])}],s=n("2877"),i={},r=Object(s["a"])(i,a,o,!1,null,null,null);r.options.__file="about.vue";t["default"]=r.exports}}]);
+//# sourceMappingURL=about.b1e21b16.js.map
---
+++ precache-manifest.7f1348f2c8867524c858cd6650c65caf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/pwa_test/robots.txt"
+  },
+  {
+    "revision": "859eacc480f25650a49f",
+    "url": "/pwa_test/js/chunk-vendors.272e5479.js"
+  },
+  {
+    "revision": "0ec74c6cc5a4646dc4c2",
+    "url": "/pwa_test/js/app.466800c9.js"
+  },
+  {
+    "revision": "635ed4545751954cba7e",
+    "url": "/pwa_test/js/about.b1e21b16.js"
+  },
+  {
+    "revision": "2d05d4c5b8ece4dd2da1e08d3af7f7c6",
+    "url": "/pwa_test/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/pwa_test/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "0ec74c6cc5a4646dc4c2",
+    "url": "/pwa_test/css/app.7271cbe1.css"
+  }
+];
